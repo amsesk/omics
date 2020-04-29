01e6540cd20da053fe74827796df0e4cb21406f5
amsesk <amsesk@umich.edu>
2020-04-29 18:54:19 -0400

    add some of these

---
+++ pyscripts/entrez.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+
+
+from Bio import Entrez
+from bs4 import BeautifulSoup as BS
+import re
+import sys
+sys.path.append("/home/aimzez/work/omics/pyscripts")
+import pandas as pd
+import numpy as np
+import ncbilib
+
+#%%
+# USER VARIABLES
+Entrez.email = "amsesk@umich.edu"
+BIOPROJECT = "PRJNA193498"
+LIBRARY_SOURCE_TARGET = "GENOMIC"
+
+
+#%%
+def print_sratoolkit_commands(df, outdir = "."):
+    for t in df.itertuples():
+        print (f"prefetch -O {outdir} {t.sra_accession}")
+        
+
+#%% Objectified so far
+import ncbilib
+runs = ncbilib.NcbiSraExperimentPackageSet.from_bioproject(BIOPROJECT)
+runs = runs.filter("LIBRARY_SOURCE == GENOMIC")
+runs.fetch_metadata()
+df = runs.to_pandas()
+print(df.sort_values("BioSample"))
+#%%
+for line in open("/home/aimzez/work/pursuit_paper/jgi_search.txt").readlines():
+    handle = Entrez.esearch(db="biosample", term=line.strip())
+    records = Entrez.read(handle)
+    handle.close()
+    samples = BS(Entrez.efetch("biosample", id=records["IdList"]).read(), "xml")
+
+    for i in samples("BioSample"):
+        biosample = i.attrs["accession"]
+        handle = Entrez.esearch(db="sra", term=biosample)
+        sraids = Entrez.read(handle)["IdList"]
+        handle.close()
+        
+        handle = Entrez.efetch(db="sra", id=sraids)
+        sra = BS(handle.read(), "xml")
+        handle.close()
+        sra_gen = [x for x in sra("EXPERIMENT_PACKAGE") if x.LIBRARY_SOURCE.string == "GENOMIC"]
+        if len(sra_gen) != 0:
+            print(f"{'-'*40}\n{line.strip()}\n{'-'*40}")
+            print (f"mkdir {line.replace(' ','_')}")
+            print (f"cd {line.replace(' ','_')}")
+            for exp in sra_gen: 
+                out = [
+                    exp.EXPERIMENT.attrs["accession"], 
+                    exp.EXPERIMENT.PLATFORM.string, 
+                    i.Owner.find("Name").string, 
+                    exp.LIBRARY_LAYOUT
+                    ]
+                print(f"prefetch -O . {out[0]}")
+                
+            print("------------------------------------")
+            
+
+    
+#print(samples.prettify())
+#%%
+
+
+
+
+
+
+
+
+
+#%%
+##############################################
+################# OLD CODE ###################
+##############################################
+#%% ENTREZ HANDLE
+bs_to_strain = {acc: run.BioSample for acc,run in runs}
+handle = Entrez.efetch(db="biosample", id = list(bs_to_strain.values()), maxret=100)
+sample_xml = BS(handle.read(), "xml")
+handle.close()
+#%%
+for acc,bs in bs_to_strain.items():
+    s = sample_xml.select(f'BioSample[accession={bs}]')
+    s = s[0]
+    try:    
+        strain = s.select('Attribute[attribute_name="strain"]')[0].string
+    except IndexError:
+        strain = s.select('Attribute[attribute_name="Strain"]')[0].string
+    try:
+        host = s.select('Attribute[display_name="host"]')[0].string
+    except IndexError:
+        host = np.nan
+    bs_to_strain[acc] = (bs, strain, host)
+    
+'''
+for s in (sample_xml.find_all("BioSample")):
+    acc = s.attrs["accession"]
+    try:    
+        strain = s.select('Attribute[attribute_name="strain"]')[0].string
+    except IndexError:
+        strain = s.select('Attribute[attribute_name="Strain"]')[0].string
+    try:
+        host = s.select('Attribute[display_name="host"]')[0].string
+    except IndexError:
+        host = np.nan
+    
+    bs_to_strain[acc] = strain
+'''
+    
+#%%
+#genomic_runs = [x for x in runs if x.LIBRARY_SOURCE.string == "GENOMIC"]
+rundict = {
+    "sra_accession": None,
+    "biosample": None,
+    "strain": None
+    }
+runs_ldict = []
+for run in runs:
+    experiment = run.find("EXPERIMENT")
+    rd = dict(rundict)
+    rd["sra_accession"] = experiment.attrs["accession"]
+    
+    assert len(run.find_all("EXPERIMENT")) == 1, f"More than one SRA experiment associated with {rd['sra_accession']}"
+    
+    #print(run.LIBRARY_SOURCE.string)
+    if run.LIBRARY_SOURCE.string == LIBRARY_SOURCE_TARGET:
+        biosample = get_biosample(run.select("EXTERNAL_ID"))
+
+        if biosample is not None:
+            rd["biosample"] = biosample
+            runs_ldict.append(rd)
+        
+        else:
+            
+            try:
+                strain = run.select('EXTERNAL_ID[label]')[0].string.split(" ")[2]
+                rd["strain"] = strain
+                runs_ldict.append(rd)
+                print(f"[WARNING]: No BioSample associated with SRA record, inferring `{strain}` from label: {rd['sra_accession']}")
+                
+            except IndexError:
+                print(f"[SKIPPING] No BioSample associated with SRA record, and CANNOT infer from label: {rd['sra_accession']}")
+            #'''
+    
+    else:
+        pass
+    
+df = pd.DataFrame(runs_ldict)
+
+#%%
+df2 = pd.DataFrame(ldict)
+df2 = pd.DataFrame(ldict).drop_duplicates()
+
+#%%
+df = df.merge(df2, on = "biosample", how="left")
+df["strain"] = df.strain_x.combine(df.strain_y, func = lambda x,y: x if pd.isna(y) else y )
+df = df.drop(["strain_x", "strain_y"], axis=1)
+df = df.sort_values("strain").reset_index()
+
+#%%
+print_sratoolkit_commands(df)
+
+#%%
+'''
+new = list(zip(acc, samples, strains))
+for samp in new:
+    print(f"prefetch -O . {samp[0]}")
+
+#%% ENTREZ HANDLE
+handle = Entrez.esearch(db="bioproject", term="PRJNA193498", retmax=1000)
+records = Entrez.read(handle)
+bp_ids = records["IdList"]
+handle.close()
+#%%
+
+samples = [x.LocusTagPrefix.attrs["biosample_id"] for x in proj_records if x.LocusTagPrefix is not None and "biosample_id" in x.LocusTagPrefix.attrs]
+print(len(samples))
+#proj_LTP = [(x.LocusTagPrefix.attrs["biosample_id"], x.LocusTagPrefix.string) for x in proj_records]
+#handle.close()
+#%%
+for i,run in enumerate(records.find_all("RUN_SET")):
+    out = [run.IDENTIFIERS.string,  run.Member.attrs["organism"], run.Member.attrs["sample_title"][2:]]
+    print (f"mv {out[0]}_1.fastq {out[1].replace(' ','_')}_{out[2]}_1_{i+1}.fastq")
+    print (f"mv {out[0]}_2.fastq {out[1].replace(' ','_')}_{out[2]}_2_{i+1}.fastq")
+'''
+#%%
---
+++ pyscripts/ncbilib.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 11:23:26 2020
+
+@author: aimzez
+"""
+import re
+import ast
+import numpy as np
+import pandas as pd
+from Bio import Entrez
+from bs4 import BeautifulSoup as BS
+
+class NcbiSraExperimentPackageSet (object):
+    def __init__(self, index):
+        self.index = index
+        self.META = ["sample_name", "geo_loc_name", "isolation_source", "strain", "host"]
+    
+    def __iter__ (self):
+        yield from self.index.items()
+        
+    def from_soup (soup, delim = "EXPERIMENT_PACKAGE"):
+        index = dict()
+        for run in soup.find_all(delim):
+
+            acc = run.find("EXPERIMENT").attrs["accession"]
+            
+            if acc not in index:
+                index[acc] = NcbiSraExperimentPackage(acc, run)
+            else:
+                raise KeyError(f"Experiment {exp} duplicated.")
+        
+        return NcbiSraExperimentPackageSet(index)
+    
+    def from_bioproject (bioproject, delim = "EXPERIMENT_PACKAGE"):
+        handle = Entrez.esearch(db="sra", term=bioproject, retmax=1000)
+        
+        records = Entrez.read(handle)
+        sra_ids = records["IdList"]
+        handle.close()
+        
+        handle = Entrez.efetch(db="sra", id=sra_ids)
+        sra_records = BS(handle.read(), "xml")
+        handle.close()
+        
+        sra_accessions = [record.attrs["accession"] for record in sra_records("EXPERIMENT")]
+        
+        handle = Entrez.efetch(db="sra", id=sra_accessions)
+        sra_records = BS(handle.read(), "xml")
+        handle.close()
+        
+        return NcbiSraExperimentPackageSet.from_soup(sra_records)
+    
+    def filter(self, expr):
+        s = re.search("(.*)(==|!=)(.*)", expr)
+        if s is None:
+            raise ValueError("Invalid filtering expression.")
+            
+        else:
+            attr, operand, value = (x.strip() for x in s.groups())
+        
+        new_index = dict()
+        for acc,run in self:
+            if not hasattr(run, attr):
+                raise ValueError("Invalid attribute.")
+            
+            if operand == "==":
+                if getattr(run, attr) == value:
+                    new_index[acc] = run
+            elif operand == "!=":
+                if getattr(run, attr) != value:
+                    new_index[acc] = run
+        
+        return NcbiSraExperimentPackageSet(new_index)
+    
+    def fetch_metadata (self):
+        biosamples = [x.BioSample for _,x in self]
+        handle = Entrez.efetch(db="biosample", id = biosamples, maxret=100)
+        sample_xml = BS(handle.read(), "xml")
+        handle.close()
+        
+        for _,run in self:
+            s = sample_xml.select(f'BioSample[accession={run.BioSample}]')
+            s = s[0]
+            
+            meta_dict = dict()
+            for m in self.META:
+                try:
+                    meta_dict[m] = s.select(f'Attribute[harmonized_name="{m}"]')[0].string
+                except IndexError:
+                    meta_dict[m] = np.nan
+
+            try:
+                meta_dict["taxonomy_name"] = s.Description.Organism.attrs["taxonomy_name"]
+            except:
+                meta_dict["taxonomy_name"] = np.nan
+                
+            try:
+                meta_dict["title"] = s.Description.Title.string
+            except:
+                meta_dict["title"] = np.nan
+                
+            run.metadata = meta_dict
+            
+            return None
+    
+    def to_ldict(self):
+        ldict = []
+        for _,run in self:
+            run_dict = {
+                "SRX": run.srx,
+                "BioSample": run.BioSample
+                }
+            run_dict.update(run.metadata)
+            
+            ldict.append(run_dict)
+            
+        return ldict
+    
+    def to_pandas(self):
+        return pd.DataFrame(self.to_ldict)[[
+            "SRX", 
+            "BioSample", 
+            "taxonomy_name",
+            "strain",
+            "title",
+            "host",
+            "isolation_source",
+            "geo_loc_name",
+            "sample_name"
+            ]]
+        
+class NcbiSraExperimentPackage (object):
+    def __init__(self, srx, soup):
+        self.srx = srx
+        self.soup = soup
+        self.LIBRARY_SOURCE = self.soup.LIBRARY_SOURCE.string
+        self.BioSample = self.get_biosample(soup.select("EXTERNAL_ID"))
+        self.metadata = None
+    
+    def get_biosample(self, tags, attr = "namespace"):
+        if tags is None:
+            return None
+        
+        for t in tags:
+            namespace_lower = t.attrs["namespace"].lower()
+            if namespace_lower == "biosample":
+                return t.string
+            
+        print(f"[WARNING]: No BioSample associated with SRA record: {self.srx}")
+        return None
+    
+if __name__ == "__main__":
+    pass
+    
