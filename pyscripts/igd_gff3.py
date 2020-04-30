50dee401ab1dc6f2f7410b3f9ec610f275cd5cb8
amsesk <amsesk@umich.edu>
2020-04-30 15:18:06 -0400

    strang NOT agnostic

--- pyscripts/igd_gff3.py
+++ pyscripts/igd_gff3.py
@@ -13,8 +13,9 @@ if sys.argv[1] in ["-h", "--help", "-help"]:
 gff3 = pd.read_csv(sys.argv[1], sep="\t", comment="#", header=None)
 
 gff3_sub = gff3.iloc[:,[0,3,4,6]]
-gff3_sub.columns = ["contig", "start", "end", "strain"]
+gff3_sub.columns = ["contig", "start", "end", "strand"]
 
-gff3_sub["next_start"] = gff3_sub.groupby(["contig"]).start.shift(-1)
+gff3_sub = gff3_sub.sort_values(["contig", "strand"])
+gff3_sub["next_start"] = gff3_sub.groupby(["contig","strand"]).start.shift(-1)
 gff3_sub = gff3_sub.assign(igd = gff3_sub["next_start"] - gff3_sub["end"])
 gff3_sub.to_csv(sys.argv[2], sep="\t", index=False)
